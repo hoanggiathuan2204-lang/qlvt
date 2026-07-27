33f555b6befa70c1366344532ccd9ad5a48a715d
Developer <dev@example.com>
2026-07-27 12:35:12 +0700

    Fix Firebase config for Windows, Linux, and Web platforms

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,9 +1,21 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
-    show defaultTargetPlatform, TargetPlatform;
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      return const FirebaseOptions(
+        apiKey: 'AIzaSyDWCpPGAG65ffLcKyzO3_BJTqvI6dVhNK0',
+        authDomain: 'qlvt-4d1fc.firebaseapp.com',
+        appId: '1:111742463162:web:59f3678af1e7199e24e683',
+        messagingSenderId: '111742463162',
+        projectId: 'qlvt-4d1fc',
+        storageBucket: 'qlvt-4d1fc.firebasestorage.app',
+        measurementId: 'G-SWFN3TX1M9',
+      );
+    }
+
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
         return const FirebaseOptions(
@@ -33,7 +45,14 @@ class DefaultFirebaseOptions {
         );
       case TargetPlatform.windows:
       case TargetPlatform.linux:
-      case TargetPlatform.fuchsia:
+        return const FirebaseOptions(
+          apiKey: 'AIzaSyCXc2NqXW9DuJVzM7S18XUbpgNoOt38ZLA',
+          appId: '1:111742463162:android:41c6f6a7e18f6dc224e683',
+          messagingSenderId: '111742463162',
+          projectId: 'qlvt-4d1fc',
+          storageBucket: 'qlvt-4d1fc.firebasestorage.app',
+        );
+      default:
         throw UnsupportedError(
           'DefaultFirebaseOptions have not been configured for this platform. '
           'Please add the necessary config files to the app.',
